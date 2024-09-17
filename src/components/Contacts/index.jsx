146bc9f9d7777d1934c3cb2f3a88e8f6acf38258
Gleison-dev <gleisonrgomes@gmail.com>
2024-09-17 15:03:42 -0300

    feat: components contacts created with links and icons

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,7 +1,27 @@
+import Icons from "../Icons/index";
+import gmail from "../../assets/gmail.png";
+import linkedin from "../../assets/linkedin.svg";
+import github from "../../assets/github.svg";
+
 export default function Contacts() {
-    return (
-        <>
-            <div></div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex justify-center mt-32">
+        <h1 className="text-3xl text-white border-b-2 border-olivine">Contatos</h1>
+      </div>
+      <div className="flex justify-evenly mt-32 bg-mindaro p-10">
+        <a href="mailto:gleisonrgomes@gmail.com">
+          {" "}
+          <Icons image={gmail} alt="Ícone do Gmail" />{" "}
+        </a>
+        <a href="https://www.linkedin.com/in/gleison-ribeiro-dev/">
+          {" "}
+          <Icons image={linkedin} alt="Ícone do Linkedin" />{" "}
+        </a>
+        <a href="https://github.com/Gleison-dev">
+          <Icons image={github} alt="Ícone do GitHub" />
+        </a>
+      </div>
+    </>
+  );
 }
